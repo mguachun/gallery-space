6b743e5bb10208e7c715cfbff2ff153bed6c3fef
Melissa Guachun <1212malka@gmail.com>
2021-06-03 13:47:44 -0400

    added before action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-
+  before_action :redirect_if_not_logged_in, only: [:show]
   def new
     @user = User.new
   end
@@ -8,7 +8,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       session[:user_id] = @user.id
-      redirect_to user_path
+      redirect_to user_path(@user)
     else 
       render :new
   end
