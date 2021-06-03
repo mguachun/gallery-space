72116cfe71d5f6eaf7e139ab9ae0463162b27af3
Melissa Guachun <1212malka@gmail.com>
2021-06-03 11:56:52 -0400

    route change

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
-  # get 'comments/new'
-  # get 'comments/create'
-  # get 'works/new'
-  # get 'works/create'
-  # get 'users/new'
-  # get 'users/create'
+  get 'comments/new'
+  get 'comments/create'
+  get 'works/new'
+  get 'works/create'
+  get 'users/new'
+  get 'users/create'
 
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
@@ -13,8 +13,6 @@ Rails.application.routes.draw do
   get '/signup', to: 'users#new'
   post '/signup', to: 'user#create'
   resources :users 
-
-
   resources :works
   resources :comments, only: [:new, :index, :create, :edit]
 
