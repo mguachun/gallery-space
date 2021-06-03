6ea64006464005d2f33e4fc96c23d20989dedfd5
Melissa Guachun <1212malka@gmail.com>
2021-06-03 12:38:58 -0400

    added user validations

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,34 @@
 class ApplicationController < ActionController::Base
     include ApplicationHelper 
-    private
+    private 
+    def logged_in?
+        !!session[:user_id]
+    end
+
+    def current_user
+        User.find_by_id(session[:user_id])
+    end
+
+    def find_user
+        @user = User.find_by_email(params[:email])
+    end
+
+    def redirect_if_logged_in
+        if logged_in?
+            redirect "/"
+        end
+    end
 
     def redirect_if_not_logged_in
-        redirect_to '/login' if !logged_in?
+        unless logged_in?
+            redirect "/login"
+        end
     end
+    
+
 end
+
+
+
+
+
