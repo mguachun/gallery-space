b0af8eb34c7ff77dd42fc0caeba08f5cbe258787
Melissa Guachun <1212malka@gmail.com>
2021-06-02 21:24:10 -0400

    added has_secure_password and validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,10 @@
 class User < ApplicationRecord
+    has_secure_password
+
     has_many :works
     has_many :comments, through: works
+
+    validates :email, presence: true, uniqueness: true
+    
+
 end
