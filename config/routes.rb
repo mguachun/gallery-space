ad0b3102bf67827427321fb972b58d47eff3666a
Melissa Guachun <1212malka@gmail.com>
2021-06-03 00:22:34 -0400

    added routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,22 @@
 Rails.application.routes.draw do
-  get 'comments/new'
-  get 'comments/create'
-  get 'works/new'
-  get 'works/create'
-  get 'users/new'
-  get 'users/create'
+  # get 'comments/new'
+  # get 'comments/create'
+  # get 'works/new'
+  # get 'works/create'
+  # get 'users/new'
+  # get 'users/create'
+
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/login', to: 'sessions#destroy'
+
+  get '/signup', to: 'users#new'
+  post '/signup', to: 'user#create'
+  resources :users 
+
+
+  resources :works
+  resources :comments, only: [:new, :index, :create, :edit]
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
