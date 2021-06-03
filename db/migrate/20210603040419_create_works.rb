e6909a2846e7750b3d494abab7dbadc8c6519eca
Melissa Guachun <1212malka@gmail.com>
2021-06-03 00:22:06 -0400

    works table

---
+++ db/migrate/20210603040419_create_works.rb
@@ -0,0 +1,15 @@
+class CreateWorks < ActiveRecord::Migration[6.1]
+  def change
+    create_table :works do |t|
+      t.string :title
+      t.string :artist
+      t.integer :year
+      t.string :medium
+      t.integer :user_id
+      t.integer :comment_id
+
+
+      t.timestamps
+    end
+  end
+end
