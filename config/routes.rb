a61737a696a881a9d8a2cc90fccb6b0cb14cdc6d
Melissa Guachun <1212malka@gmail.com>
2021-06-02 21:25:43 -0400

    creating routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  get 'works/new'
+  get 'works/create'
+  get 'users/new'
+  get 'users/create'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
