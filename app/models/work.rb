3fe526bf9f9ddaae3804c5d9f99c65d426073915
Melissa Guachun <1212malka@gmail.com>
2021-06-07 12:42:28 -0400

    added relationship

--- app/models/work.rb
+++ app/models/work.rb
@@ -1,6 +1,8 @@
 class Work < ApplicationRecord
     belongs_to :user
     belongs_to :comments
+    has_many :comments
+    has_many :users, through: :comments
 
     validates :artist, presence: true
     validates :name, presence: true
