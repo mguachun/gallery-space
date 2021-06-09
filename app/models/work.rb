6e8bad1fc4201c106c8b776c56fe44c3f1c7e5f7
Melissa Guachun <1212malka@gmail.com>
2021-06-09 12:52:29 -0400

    added validations

--- app/models/work.rb
+++ app/models/work.rb
@@ -4,8 +4,9 @@ class Work < ApplicationRecord
     has_many :comments
     has_many :users, through: :comments
 
+    validates :title, presence: true
     validates :artist, presence: true
-  
+    validates :year, presence: true
     validates :medium, presence: true
 
 end
