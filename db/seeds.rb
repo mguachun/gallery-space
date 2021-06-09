745ca8294618c8adcf0be2758fb70f807eb2e0c8
Melissa Guachun <1212malka@gmail.com>
2021-06-09 12:52:16 -0400

    seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -18,15 +18,25 @@ users = User.create([
 { email: "walpole@gmail.com", password:"bobbob"}, 
 { email: "hg2020@gmail.com", password:"briggs"} ])
 
-works = Work.create([
-{title: "Mona Lisa", artist: "Leonard da Vinci" , year: 1503 , medium: "oil" },
-{title:"Guernica", artist: "Pablo Picasso", year:1937 , medium:"oil" },
-{title: "The Birth of Venus", artist: "Sandro Botticelli" , year: 1485 , medium:"tempera" },
-{title: "The Kiss", artist: "Gustav Klimt", year:1907, medium:"oil"},
-{title: "American Gothic", artist: "Grant Wood", year: 1930 , medium: "oil"},
-{title: "Nighthawks", artist: "Edward Hopper", year:1942, medium: "oil" },
-{title: "The Nightwatch", artist: "Rembrandt", year: 1642 , medium: "oil" },
-{title: "Campbell's Soup Cans", artist: "Andy Warhol" , year: 1961 , medium: "syntheic polymer paint"},
-{title: "The Great Wave off Kanagawa", artist:"Hokusai", year: 1820 , medium: "print" },
-{title: "David", artist:"Michelangelo", year: 1501 , medium: "marble" } ])
+Work.create([{ title: "Mona Lisa", artist: "Leonard da Vinci" , year: "1503" , medium: "oil"},
+{ title:"Guernica", artist: "Pablo Picasso", year: "1937", medium:"oil"},
+{ title: "The Birth of Venus", artist: "Sandro Botticelli", year: "1485", medium:"tempera"},
+{ title: "The Kiss", artist: "Gustav Klimt", year: "1907", medium:"oil"},
+{ title: "American Gothic", artist: "Grant Wood", year: "1930" , medium: "oil"},
+{ title: "Nighthawks", artist: "Edward Hopper", year: "1942", medium: "oil" },
+{ title: "The Nightwatch", artist: "Rembrandt", year: "1642" , medium: "oil" },
+{ title: "Campbell's Soup Cans", artist: "Andy Warhol" , year: "1961" , medium: "syntheic polymer paint"},
+{ title: "The Great Wave off Kanagawa", artist:"Hokusai", year: "1820" , medium: "print"},
+{ title: "David", artist:"Michelangelo", year: "1501" , medium: "marble"}])
 
+
+Comment.create([{ headline: "Comment 1", description: "I think this piece is in France", user_id:1},
+{ headline: "Comment 2", description: "This is actually a huge work of art in person", user_id:2},
+{ headline: "Comment 3", description: "Why is this artist known for this piece?", user_id:3},
+{ headline: "Comment 4", description: "Would this be more iconic if done in another medium?", user_id:4},
+{ headline: "Comment 5", description: "What does the title mean?", user_id:5},
+{ headline: "Comment 6", description: "me lol", user_id:6},
+{ headline: "Comment 7", description: "Game of thrones reference", user_id:7},
+{ headline: "Comment 8", description: "I could've made that!", user_id:8},
+{ headline: "Comment 9", description: "what artists were inspired by this piece?", user_id:9},
+{ headline: "Comment 10", description: "proportions", user_id:10}])
