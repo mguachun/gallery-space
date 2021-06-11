16d2891f85bbe9093106eda87f2eaa4d3b7e73bc
Melissa Guachun <1212malka@gmail.com>
2021-06-11 13:42:56 -0400

    schema change and update

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_09_043745) do
+ActiveRecord::Schema.define(version: 2021_06_11_172844) do
 
   create_table "comments", force: :cascade do |t|
     t.string "headline"
@@ -35,6 +35,7 @@ ActiveRecord::Schema.define(version: 2021_06_09_043745) do
     t.string "medium"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id"
   end
 
 end
