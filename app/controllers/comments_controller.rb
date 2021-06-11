a4ff3152bdf23f5566b6e39fcc6a231319e2138f
Melissa Guachun <1212malka@gmail.com>
2021-06-11 13:43:38 -0400

    added to comment controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -8,12 +8,42 @@ class CommentsController < ApplicationController
     @comment = Comment.find(params[:id])
   end
 
-
   def new
     @comment = Comment.new
   end
 
   def create
-  
+    @comment = Comment.new(comment_params)
+    if @comment.save
+      redirect_to comments_path
+    else 
+      render :new
+    end
+  end
+
+  def edit 
+    @comment = Comment.find(params[:id])
+  end
+
+  def update
+    @comment = Comment.find(params[:id])
+    @comment.update(shoe_params)
+    if @comment.valid?
+        redirect_to commentss_path 
+    else
+        render :edit 
+    end
+  end
+
+  def destroy
+    @comment = Comment.find(params[:id])
+    @comment.destroy
+    redirect_to comments_path 
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:headline, :description)
   end
 end
