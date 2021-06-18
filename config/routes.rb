d31335621fd8a5cc42ec2bb7ebde49d343e1d79a
Melissa Guachun <1212malka@gmail.com>
2021-06-17 21:30:53 -0400

    added get request to log out

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,10 @@ Rails.application.routes.draw do
  
   get '/signup' => 'users#new'
   post '/signup' => 'users#create'
+
   get '/login' => 'sessions#new'
   post '/login' => 'sessions#create'
+  get '/logout' => 'sessions#destroy'
   post '/logout' => 'sessions#destroy'
 
   resources :users 
