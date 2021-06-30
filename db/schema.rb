4d1afdec77ecc0d8813a192fe1066eb67a7e5b0a
Melissa Guachun <1212malka@gmail.com>
2021-06-30 18:37:11 -0400

    schema change

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_11_172844) do
+ActiveRecord::Schema.define(version: 2021_06_30_222008) do
 
   create_table "comments", force: :cascade do |t|
     t.string "headline"
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(version: 2021_06_11_172844) do
     t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "uid"
   end
 
   create_table "works", force: :cascade do |t|
