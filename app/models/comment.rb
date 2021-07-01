1b088ee8bfabb5baecd2d4d201ae8d1bc0774502
Melissa Guachun <1212malka@gmail.com>
2021-07-01 15:52:15 -0400

    scope

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,11 @@
 class Comment < ApplicationRecord
     belongs_to :work
     belongs_to :user
+
+ scope :latest_user_comments, -> {where ("comments.last(3)")}
+
+
+    def self.latest_user_comments
+        comments = Comment.last(3)
+    end
 end
